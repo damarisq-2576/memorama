49585822b3b4f015bb82129c85f84af989be1a12
damarisq-2576 <damaris.quiroz@uala.com.ar>
2020-08-09 21:57:55 -0300

    ngdirec obj

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+interface CardInterface {
+  id: number,
+  idCard: number,
+  revealed: boolean,
+  touch: boolean,
+  active: boolean
+}
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
@@ -9,41 +17,42 @@ export class BoardComponent implements OnInit {
 
   //global Board vars
   howMany = 20;
-  colNum = 4;
-  allCards = []; //  Y si se hace obj ?
-
+  allCards: Array<CardInterface>;
 
   //Player vars
   turn: boolean; // true-> Player1 || false -> Player2
   winner = false;
-  currentTurns: number; // each player flip card
   pts = {
     ptsPlayer1: 0,
     ptsPlayer2: 0
   }
   whoWin: string;
   timePlayer = 6;
-  timeLeft: number;
+  timeLeft: number;
   interval;
-  // Reset handle
-  unflipAll: boolean;
-  // Temporary vars
-  firstCardInfo: any;
-  unflipCards = [];
 
-  initArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
   constructor() { }
 
   ngOnInit() {
     // init values
     this.turn = true;
-    this.currentTurns = 2;
     this.timeLeft = this.timePlayer;
-    this.allCards = this.shuffleCards(this.initArray);
-    console.log(this.allCards)
+    this.allCards = this.shuffleCards(this.createCards());
     this.startTimer();
   }
 
+  createCards() {
+    const initArr = Array.from(Array(this.howMany / 2), (_, index) => ({
+      id: index,
+      idCard: index + 1,
+      revealed: false,
+      touch: true,
+      active: false
+    }));
+    //deep level copy
+    const cloned = initArr.map(x => Object.assign({}, x));
+    return [...initArr, ...cloned.reverse()];
+  }
 
   shuffleCards(list) {
     let ctrl = list.length;
@@ -52,47 +61,63 @@ export class BoardComponent implements OnInit {
     while (ctrl > 0) {
       index = Math.floor(Math.random() * ctrl);
       ctrl--;
-      temp = list[ctrl];
-      list[ctrl] = list[index];
-      list[index] = temp;
+      temp = list[ctrl].idCard;
+      list[ctrl].idCard = list[index].idCard;
+      list[index].idCard = temp;
     }
     return list;
   }
 
 
   selectedCard(activeCard: any) {
-    this.currentTurns--;
-    this.unflipAll = false;
-    if (this.currentTurns == 1) {
-      // Store first click info
-      this.firstCardInfo = activeCard;
-    }
+    let contTurns = 0;
+    let position = [];
+    let cardsHistory = activeCard.filter((x, i) => {
+      if (x.revealed === true && x.active === true) {
+        contTurns++;
+        position.push(i)
+        return x;
+      }
+    });
 
-    if (this.currentTurns == 0) {
-      // Time to compare
-      if (activeCard.card == this.firstCardInfo.card) {
+
+
+    if (contTurns === 2  && (cardsHistory[0].id != cardsHistory[1].id)){
+
+      let blockCards = activeCard.filter((x, i) => {
+        if (x.revealed === false || x.active === false) {
+          x.touch = false;
+          return x;
+        }
+      });
+      if (cardsHistory[0].idCard == cardsHistory[1].idCard) {
         if (this.turn) {
           this.pts.ptsPlayer1++;
         } else {
           this.pts.ptsPlayer2++;
         }
         this.getWinner();
+        activeCard[position[0]].active = activeCard[position[1]].active = false;
+        blockCards.map(x => x.touch=true);
       } else {
-        this.firstCardInfo.revealed = activeCard.revealed = false;
-        // send cards to be unflipped
-        this.unflipCards = [this.firstCardInfo, activeCard];
-        // change player turn
-        this.turn = !this.turn;
-        this.resetTimer();
+        setTimeout(() => {
+          activeCard[position[0]].active = activeCard[position[1]].active = false;
+          activeCard[position[0]].revealed = activeCard[position[1]].revealed = false;
+          // change player turn
+          this.turn = !this.turn;
+          this.resetTimer();
+
+          blockCards.map( x => {return x.touch=true});
+        }, 1500)
 
       }
-      this.currentTurns = 2;
     }
   }
 
   getWinner() {
     if ((this.pts.ptsPlayer1 + this.pts.ptsPlayer2) == (this.howMany / 2)) {
       this.winner = true;
+      clearInterval(this.interval);
       if (this.pts.ptsPlayer1 > this.pts.ptsPlayer2) {
         this.whoWin = "Player 1 is the winner";
         return
@@ -108,13 +133,10 @@ export class BoardComponent implements OnInit {
   resetPlay() {
     //init Vars
     this.whoWin = "";
-    this.allCards = this.shuffleCards(this.initArray);
     this.winner = false;
     this.pts.ptsPlayer1 = this.pts.ptsPlayer2 = 0;
-    this.currentTurns = 2;
-    this.unflipAll = true;
-    this.unflipCards = []
     this.resetTimer();
+    this.allCards = this.shuffleCards(this.createCards());
   }
 
   startTimer() {
@@ -122,8 +144,14 @@ export class BoardComponent implements OnInit {
       if (this.timeLeft > 0) {
         this.timeLeft--;
       } else {
-        this.turn =!this.turn;
+        this.turn = !this.turn;
         // flip cards if necessary
+        this.allCards.map((obj) => {
+          if (obj.revealed === true && obj.active === true) {
+            obj.active = false;
+            obj.revealed = false;
+          }
+        })
         this.timeLeft = this.timePlayer;
       }
     }, 1000)
--- src/app/handle-cards/handle-cards.component.ts
+++ src/app/handle-cards/handle-cards.component.ts
@@ -1,77 +1,30 @@
-import { Component, OnInit, Input, Output, EventEmitter, Inject, SimpleChanges } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+interface CardInterface {
+    id: number,
+    idCard: number,
+    revealed: boolean,
+    touch: boolean,
+    active: boolean
+}
 
 @Component({
   selector: 'app-handle-cards',
   templateUrl: './handle-cards.component.html',
   styleUrls: ['./handle-cards.component.scss']
 })
-export class HandleCardsComponent implements OnInit {
+export class HandleCardsComponent {
+  @Input() allCards:CardInterface;
+  @Output() activeCard: EventEmitter<CardInterface> = new EventEmitter();
 
-  @Input() columnNumb;
-  @Input() start;
-  @Input() allCards;
-  @Input() set unflipAll(unflip) {
-    if (unflip) {
-      for (let ctr = 0; ctr < this.allCards.length; ctr++) {
-        document.getElementById("" + ctr + "").classList.remove("flipped");
-      }
-    }
-  };
+  constructor() { }
 
-  @Output() activeCard: EventEmitter<any> = new EventEmitter();
-  // Need to get input changes
-  @Input() set unflip(unflipCards) {
-    if (unflipCards.length > 0) {
-      this.unrotateCards(unflipCards)
-    }
-  };
 
-  end: number;
-  paintCards: any;
-  cont: number;
-  constructor(@Inject(DOCUMENT) document) { }
-
-  ngOnInit() {
-    this.end = this.start + this.columnNumb;
-    this.paintCards = this.allCards.slice(this.start, this.end);
-    this.cont = this.start;
+  rotateCard(id) {
+    // Set properties and correct id value to compare
+    this.allCards[id].id = id;
+    this.allCards[id].revealed = true;
+    this.allCards[id].active = true;
+    this.activeCard.emit(this.allCards);
   }
 
-
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes)
-      this.paintCards = this.allCards.slice(this.start, this.end);
-  }
-
-  rotateCard(card, id, e) {
-    // make sure click is in front div
-    if (parseInt(e.currentTarget.id) === id) {
-      // toggle comparison
-      debugger
-      if (!e.currentTarget.classList.contains("flipped")) {
-        e.currentTarget.classList.add("flipped");
-
-        let cardInfo = {
-          "card": card,
-          "idDiv": id,
-          "flipped": true,
-          "evt": e.currentTarget
-        }
-        this.activeCard.emit(cardInfo);
-      }
-    }
-  }
-
-  unrotateCards(unflipCards = []) {
-    Object.entries(unflipCards).forEach(([key, unflipCard]) => {
-      if (unflipCard.revealed == false) {
-        setTimeout(() => {
-          unflipCard.evt.classList.remove("flipped");
-        }, 1500)
-      }
-    });
-  }
 }
